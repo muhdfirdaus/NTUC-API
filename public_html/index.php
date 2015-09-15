6587fe0a18d2c3e2de9f435bf20eaf36c8a4bb30
Khairul <khairul.ghazali@misatravel.com>
2015-09-15 18:06:07 +0800

    this close #12

--- public_html/index.php
+++ public_html/index.php
@@ -56,8 +56,15 @@ $app->post('/api/updates/', function () use($app){
 	if (($csv = retrieveUserInfo($apiKey)) === FALSE) {
 		$app->halt(401,json_encode(array('status' => 0,'message' => 'Invalid API key')));
 	}
+	
+	 $timestamp = $app->request->headers->get('timestamp');
+ 
+ if (!strlen($timestamp)) {
+  $app->halt(400,json_encode(array('status' => 2,'message' => 'Please specify fingerprint')));
+ } 
+	
 	$fp = fingerhash($apiKey,$timestamp,$nric,$amount,$date,$source);
-	$timestamp = $app->request->headers->get('timestamp');
+	$timestamp = $app->request->headers->get('timestamp');	
 	$fingerprint = $app->request->headers->get('fingerprint');
 	$timestamp =  intval($timestamp);
 	$current = intval(time());
